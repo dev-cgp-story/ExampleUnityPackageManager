e7fbd577df696feb89ad3cf31512329d899335eb
Kang MyeongShin <myeongshin.kang@line.games>
2019-02-22 01:25:56 +0900

    init

---
+++ Editor/MyMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class MyMenu
+{
+    [MenuItem("MyMenu/MyMenuItem")]
+    public static void MyMenuItem() 
+    {
+
+    }
+}
